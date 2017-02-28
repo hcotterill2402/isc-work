29de0fa22bdecd3d1db2da76f84b9433702a82ac
user01 <user01@localhost.localdomain>
2017-02-28 16:36:00 +0000

    While loops in Python

---
+++ python/while.py
@@ -0,0 +1,16 @@
+#While loops in Python
+#Control Flow, excercise 1
+
+#1
+
+#never ending while loop!
+#x=5
+#while x>0:
+#    print x
+#    x+=1
+
+#While loop that never gets called
+x=5
+while x<0:
+    print x
+    x+=1
