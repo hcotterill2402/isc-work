c505bdb725715c63cc72d20f48d293a023afb91a
user01 <user01@localhost.localdomain>
2017-02-28 15:59:54 +0000

    version 1

---
+++ python/basics.py
@@ -0,0 +1,24 @@
+
+
+#Basic Python commands and stuff
+#Don't run this file! This is just for reference
+
+##1
+#course='python' #need to write a string with ' '
+#rating=5
+#print course
+#print rating
+##prints values of course and rating to screen
+
+#2 writing pythagoras' theorem
+b=6 
+c=4
+a=(b**2 + c**2)**0.5
+print a
+
+print type(a) #prints the type, ie int, float, string
+print type(b)
+print type(c)
+
+int(a)
+print type(a)
