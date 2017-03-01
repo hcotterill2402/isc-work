83f5604acdc2871a499897fbd39f6695413d30f1
user01 <user01@localhost.localdomain>
2017-03-01 09:16:34 +0000

    testing the truth of a variable

---
+++ python/truth.py
@@ -0,0 +1,10 @@
+#Testing truth of statements
+#excercise 3 of Control Flow
+
+#3
+
+x=[] #change this to test if different values of x are 'true'
+if x: print x, "is True"
+
+#0's give no response, therefore they are False
+
